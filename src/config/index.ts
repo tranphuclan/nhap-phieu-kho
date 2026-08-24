2da1cd39d5d4840700f0efa01af744c40575d204
Trần Phúc Lân <tranphuclan183@gmail.com>
2026-08-24 17:48:14 +0700

    deploy cloud

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,18 @@
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-const path = ".env"
-console.table({ env_path: path })
-config({ path })
+// const path = ".env"
+// console.table({ env_path: path })
+// config({ path })
 
-const getEnvString = (key: string) => {
-    if (!process.env[key]) throw new Error(`${key} must be provided`)
-    return process.env[key] as string
-}
+// const getEnvString = (key: string) => {
+//     if (!process.env[key]) throw new Error(`${key} must be provided`)
+//     return process.env[key] as string
+// }
 
-export const DATABASE_URL = getEnvString("DATABASE_URL")
-export const DATABASE_NAME = getEnvString("DATABASE_NAME")
-export const PORT = Number(getEnvString("PORT"))
+// export const DATABASE_URL = getEnvString("DATABASE_URL")
+// export const DATABASE_NAME = getEnvString("DATABASE_NAME")
+// export const PORT = Number(getEnvString("PORT"))
+
+export const DATABASE_URL = process.env.DATABASE_URL
+export const DATABASE_NAME = process.env.DATABASE_NAME
+export const PORT = Number(process.env.PORT)
